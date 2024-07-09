c8070e9fc14990db75e0b80274204f0a8d19cdb6
Paweł <150943265+Flaige@users.noreply.github.com>
2024-07-09 22:44:29 +0200

    Update fetchCountries.js

--- fetchCountries.js
+++ fetchCountries.js
@@ -1,8 +1,10 @@
-export async function fetchCountries(name) {
-    const url = `https://restcountries.com/v2/name/${name}?fields=name;capital;population;flags;languages`;
+const Countries_API_URL = "https://restcountries.com/v3.1/name/";
 
+export async function fetchCountries(name) {
     try {
-        const response = await fetch(url);
+        const response = await fetch(
+            `${Countries_API_URL}${name}?fields=name,capital,population,flags,languages`
+        );
 
         if (!response.ok) {
             if (response.status === 404) {
@@ -15,11 +17,11 @@ export async function fetchCountries(name) {
         const countries = await response.json();
 
         return countries.map(country => ({
-            name: country.name,
-            capital: country.capital,
+            name: country.name.official,
+            capital: country.capital ? country.capital[0] : 'Brak',
             population: country.population,
             flag: country.flags.svg,
-            languages: Object.values(country.languages).join(', ')
+            languages: country.languages ? Object.values(country.languages).join(', ') : 'Brak'
         }));
     } catch (error) {
         console.error('Error fetching countries:', error);
